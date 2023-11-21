e6f0eef7a366e5fbb722b767424591d5fe87016c
Munawar <134474387+munawar3206@users.noreply.github.com>
2023-11-21 13:00:44 +0530

    completed{validation added}

--- lib/views/add.dart
+++ lib/views/add.dart
@@ -23,130 +23,124 @@ class AddUser extends StatelessWidget {
         key: formkey,
         child: Padding(
           padding: const EdgeInsets.all(20.0),
-          child: Column(children: [
-            CupertinoTextField(
-              placeholderStyle: TextStyle(color: blue),
-              prefix: Icon(Icons.person),
-              placeholder: "Name",
-              controller: techprovider.studname,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(5),
-                shape: BoxShape.rectangle,
-                border: Border.all(),
-              ),
-              // decoration: const InputDecoration(
-              //   icon: Icon(
-              //     Icons.person,
-              //     color: black,
-              //   ),
-              //   border: OutlineInputBorder(),
-              //   label: Text("Devops Name"),
-              // ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            CupertinoTextField(
-              placeholderStyle: TextStyle(color: blue),
-              prefix: Icon(Icons.class_),
-              placeholder: "Batch",
-              controller: techprovider.studbatch,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(5),
-                shape: BoxShape.rectangle,
-                border: Border.all(),
-              ),
-              // decoration: const InputDecoration(
-              //     icon: Icon(
-              //       Icons.class_,
-              //       color: black,
-              //     ),
-              //     border: OutlineInputBorder(),
-              //     label: Text("Batch")),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            CupertinoTextField(
-              placeholderStyle: TextStyle(color: blue),
-              prefix: Icon(Icons.phone),
-              placeholder: "Phone Number",
-              controller: techprovider.studphone,
-              keyboardType: TextInputType.number,
-
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(5),
-                shape: BoxShape.rectangle,
-                border: Border.all(),
-              ),
-              maxLength: 10,
-
-              //   decoration: const InputDecoration(
-              //       icon: Icon(
-              //         Icons.phone,
-              //         color: black,
-              //       ),
-              //       border: OutlineInputBorder(),
-              //       label: Text("Phone Number")),
-              //   maxLength: 10,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: DropdownButtonFormField(
-                dropdownColor: blue,
+          child: SingleChildScrollView(
+            child: Column(children: [
+              TextFormField(
+                controller: techprovider.studname,
                 decoration: const InputDecoration(
-                  label: Text(
-                    "Select Domain",
-                    style: TextStyle(color: blue),
+                  icon: Icon(
+                    Icons.person,
+                    color: black,
                   ),
+                  border: OutlineInputBorder(),
+                  label: Text("Devops Name"),
                 ),
-                items: domains
-                    .map((e) => DropdownMenuItem(
-                          value: e,
-                          child: Text(e,
-                              style: const TextStyle(
-                                  fontWeight: FontWeight.bold, color: black)),
-                        ))
-                    .toList(),
-                onChanged: (val) {
-                  techprovider.selectedGroup = val;
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Enter the valid Name";
+                  }
+                  return null;
                 },
-                borderRadius: BorderRadius.circular(25),
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            CupertinoButton(
-              color: blue,
-              onPressed: () {
-                if (formkey.currentState!.validate()) {
-                  techprovider.addtechname();
-                  techprovider.studname.clear();
-                  techprovider.studbatch.clear();
-                  techprovider.studphone.clear();
-
-                  Navigator.pop(context);
-                }
-                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                  backgroundColor: Colors.green,
-                  content: Text(
-                    "Devops Added",
-                    style: TextStyle(fontWeight: FontWeight.bold),
+              const SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                controller: techprovider.studbatch,
+                decoration: const InputDecoration(
+                    icon: Icon(
+                      Icons.class_,
+                      color: black,
+                    ),
+                    border: OutlineInputBorder(),
+                    label: Text("Batch")),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Batch required";
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                controller: techprovider.studphone,
+                keyboardType: TextInputType.number,
+                maxLength: 10,
+                decoration: const InputDecoration(
+                    icon: Icon(
+                      Icons.phone,
+                      color: black,
+                    ),
+                    border: OutlineInputBorder(),
+                    label: Text("Phone Number")),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Enter the Number";
+                  }
+                  return null;
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: DropdownButtonFormField(
+                  dropdownColor: blue,
+                  decoration: const InputDecoration(
+                    label: Text(
+                      "Select Domain",
+                      style: TextStyle(color: blue),
+                    ),
                   ),
-                  duration: Duration(seconds: 3),
-                ));
-              },
-              // style: const ButtonStyle(
-              //     minimumSize:
-              //         MaterialStatePropertyAll(Size(double.infinity, 50)),
-              //     backgroundColor: MaterialStatePropertyAll(blue)),
-              child: const Text(
-                "Submit",
-                style: TextStyle(fontSize: 20),
+                
+                  items: domains
+                      .map((e) => DropdownMenuItem(
+                            value: e,
+                            child: Text(e,
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold, color: black)),
+                          ))
+                      .toList(),
+                  onChanged: (val) {
+                    techprovider.selectedGroup = val;
+                  },
+                  borderRadius: BorderRadius.circular(25),
+                ),
               ),
-            )
-          ]),
+              const SizedBox(
+                height: 10,
+              ),
+              CupertinoButton(
+                color: blue,
+                onPressed: () {
+                  if (formkey.currentState!.validate()) {
+                    techprovider.addtechname();
+                    techprovider.studname.clear();
+                    techprovider.studbatch.clear();
+                    techprovider.studphone.clear();
+
+                    Navigator.pop(context);
+                  }
+                  //   ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                  //     backgroundColor: Colors.green,
+                  //     content: Text(
+                  //       "Devops Added",
+                  //       style: TextStyle(fontWeight: FontWeight.bold),
+                  //     ),
+                  //     duration: Duration(seconds: 3),
+                  //   ));
+                },
+                // style: const ButtonStyle(
+                //     minimumSize:
+                //         MaterialStatePropertyAll(Size(double.infinity, 50)),
+                //     backgroundColor: MaterialStatePropertyAll(blue)),
+                child: const Text(
+                  "Submit",
+                  style: TextStyle(fontSize: 20),
+                ),
+              )
+            ]),
+          ),
         ),
       ),
     );
